e79b262b5d8d91048ed3068d72942866906dcdb5
Ubuntu <ubuntu@ip-172-31-18-62.ec2.internal>
2021-04-28 18:07:06 +0000

    added in basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Created By: Jonathan Kene
+# Date: April 28, 2021
+# This does basic math
+import math
+def main():
+    print("4+4={}".format(4+4))
+    print("5-3={}".format(5-3))
+    print("3*3={}".format(3*3))
+    print("10/5={}".format(10/5))
+    print("2**3={}".format(2**3))
+    print("(8+8)/4={}".format((8+8)/4))
+    print("√25={}".format(math.sqrt(25)))
+    print ("{:.2f}.format(3.1415926))
+    print("2*2/4*100**2-100**2={}".format(2*2/4*100**2-100**2))
+
+if __name__ == "__main__":
+    main()
